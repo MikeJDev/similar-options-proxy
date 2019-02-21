d5afa836683b78663656332711c64c233465aba1
Anthony Burroughs <bonfilio.tony@gmail.com>
2019-02-21 11:29:24 -0600

    Set up 'boilerplate' (file structure, basic server, webpack/babel config, starter component rendering to page, package.json scripts)

---
+++ client/src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Axios from "axios";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Testing! you asshole</h1>
+      </div>
+    );
+  };
+};
+
+ReactDOM.render(<App />, document.getElementById('home-page'));
